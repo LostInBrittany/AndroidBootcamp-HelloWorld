96fc0d5acc97e6f31bbde4fe397c6101339b1557
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2013-09-13 03:30:43 +0200

    EventListener basique sur le bouton

--- HelloWorldWithIntentsAndEvents/src/main/java/org/gdgrennes/android/bootcamp/MainActivity.java
+++ HelloWorldWithIntentsAndEvents/src/main/java/org/gdgrennes/android/bootcamp/MainActivity.java
@@ -3,6 +3,9 @@ package org.gdgrennes.android.bootcamp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -19,5 +22,21 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Button b = (Button)findViewById(R.id.buttonOK);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),
+                        "Hello World!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
 }
